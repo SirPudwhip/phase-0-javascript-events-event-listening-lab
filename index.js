82dc7aae3bb0542bd5889a504d67a74f4cd35907
SirPudwhip <spamfolder3399@gmail.com>
2023-01-17 17:11:44 -0500

    commit tree 0f471d090c8fa27ad653aba913eec982ef951b6b

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const test = document.getElementById('button');
+
+function printAlert() {
+    alert('Oh jeez, you pushed the button!')
+}
+
+function addingEventListener(){
+    test.addEventListener('click', printAlert);
 }
