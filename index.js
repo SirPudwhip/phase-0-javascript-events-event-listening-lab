78d6e6ee63f2ec6576894805cd0df2a91c20a9e6
SirPudwhip <spamfolder3399@gmail.com>
2023-01-31 12:37:31 -0500

    Pushing updates to pass test

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+const button1 = document.getElementById('button'); 
+
+function getClicked() {
+    alert('WHOA nice button click!');
 }
+
+function addingEventListener() {
+    button1.addEventListener('click', getClicked);;
+}
+
